e22d27ce8f54ce4ceb32c79f2173f45f7f842db6
max <mcnic@users.noreply.github.com>
2024-07-24 19:13:01 -0700

    fix: order service

--- src/order/services/order.service.ts
+++ src/order/services/order.service.ts
@@ -1,5 +1,4 @@
 import { Injectable } from '@nestjs/common';
-// import { v4 } from 'uuid';
 import { Order, OrderDto } from '../models';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { plainToClass } from 'class-transformer';
@@ -8,34 +7,34 @@ import { plainToClass } from 'class-transformer';
 export class OrderService {
   constructor(private prismaService: PrismaService) {}
 
-  // async findById(orderId: string): Promise<Order> {
-  //   const order = await this.prismaService.order.findFirst({
-  //     where: {
-  //       id: orderId,
-  //     },
-  //   });
-
-  //   return plainToClass(Order, order);
-  // }
-
-  async create(dto: OrderDto): Promise<Order> {
-    const order = await this.prismaService.createOrder(dto)
+  async findById(orderId: string): Promise<Order> {
+    const order = await this.prismaService.order.findFirst({
+      where: {
+        id: orderId,
+      },
+    });
 
     return plainToClass(Order, order);
   }
 
-  //  async update(orderId, data: OrderDto) {
-  //     const findedOrder = this.findById(orderId);
+  async create(dto: OrderDto): Promise<Order> {
+    const order = await this.prismaService.createOrder(dto);
 
-  //     if (!findedOrder) {
-  //       throw new Error('Order does not exist.');
-  //     }
+    return plainToClass(Order, order);
+  }
 
-  //     const order = await this.prismaService.order.update({
-  //       where: { id: orderId },
-  //       data: {
-  //         ...data,
-  //       } as any,
-  //     });
-  //   }
+  async update(orderId, data: OrderDto) {
+    const findedOrder = this.findById(orderId);
+
+    if (!findedOrder) {
+      throw new Error('Order does not exist.');
+    }
+
+    const order = await this.prismaService.order.update({
+      where: { id: orderId },
+      data: {
+        ...data,
+      } as any,
+    });
+  }
 }
